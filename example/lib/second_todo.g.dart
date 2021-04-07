595474b726afc8f756f3dffae7d630489b39beae
余玠 <xujian.xj@alibaba-inc.com>
2021-04-07 10:45:31 +0800

    add build.yaml

--- example/lib/second_todo.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'second_todo.dart';
-
-// **************************************************************************
-// TodoReporterGenerator
-// **************************************************************************
-
-// Hey! Annotation found!
